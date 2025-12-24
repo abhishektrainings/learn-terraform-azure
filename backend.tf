561c53ec219232d3eb659e4bd2ce914fc8e602a4
Abhishek Tiwari <abhishektrainings30@gmail.com>
2025-12-24 08:34:34 +0530

    Comment out Azure backend to use HCP Terraform cloud

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name = "tfstate-rg"
-    storage_account_name = "tfstatebackupstacc"
-    container_name = "tfstate"
-    key = "rg.terraform.tfstate"
-  }
+  # Backend is commented out; using HCP Terraform cloud instead (configured in main.tf)
+  # backend "azurerm" {
+  #   resource_group_name = "tfstate-rg"
+  #   storage_account_name = "tfstatebackupstacc"
+  #   container_name = "tfstate"
+  #   key = "rg.terraform.tfstate"
+  # }
 }
