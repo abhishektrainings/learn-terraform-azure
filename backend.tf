47cbf3acf46ebc3bb08384dd6aedb30d78f9af2f
Abhishek Tiwari <abhishektrainings30@gmail.com>
2025-12-26 09:37:35 +0530

    Fix vnet resource type and migrate state to azurerm backend
    - Update module resource from azure_virtual_network to azurerm_virtual_network
    - Re-enable and configure azurerm backend for state management
    - State migrated from Terraform Cloud to Azure Storage blob
    Resolves hashicorp/azure provider not found error

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  # Backend is commented out; using HCP Terraform cloud instead (configured in main.tf)
-   backend "azurerm" {
-     resource_group_name = "tfstate-rg"
-     storage_account_name = "tfstatebackupstacc2612"
-     container_name = "tfstate"
-     key = "rg.terraform.tfstate"
-   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatebackupstacc2612"
+    container_name       = "tfstate"
+    key                  = "rg.terraform.tfstate"
+  }
 }
--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azure_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
