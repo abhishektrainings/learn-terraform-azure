6be159d60aa55fe38636a6f0f753913eddbf5e68
Abhishek Tiwari <abhishektrainings30@gmail.com>
2025-12-31 09:19:58 +0530

    updates

--- provider.tf
+++ provider.tf
@@ -1,12 +1,23 @@
+variable "client_secret" {
+}
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "=4.1.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  client_id       = "af0caf81-3f81-4c3b-ba44-9a64afcee240"
+  client_secret   = var.client_secret
+  tenant_id       = "69e2d3ca-622c-43e1-86ee-fd6a642240ac"
+  subscription_id = "992fe4cd-e4d9-4eab-9b5f-a6eb24758946"
 }
