fd598128b1b48c50ea8846df6df633dca3e6aa3d
Abhishek Tiwari <abhishektrainings30@gmail.com>
2026-01-02 08:40:10 +0530

    updates

--- provider.tf
+++ provider.tf
@@ -9,4 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  # Service Principal authentication (for CI/CD pipelines)
+  # Set these environment variables: ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
+  
+  # Alternatively, use Azure CLI authentication
+  # Run 'az login' to authenticate as a user
 }
