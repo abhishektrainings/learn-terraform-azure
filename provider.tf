f281018e3ac83c0bf1a4c6ad424f7533f7d9f5bc
Abhishek Tiwari <abhishektrainings30@gmail.com>
2026-01-19 08:33:02 +0530

    my commit

--- provider.tf
+++ provider.tf
@@ -10,13 +10,13 @@ terraform {
 provider "azurerm" {
   features {}
 
-  # Service Principal authentication (for CI/CD pipelines)
-  # Explicitly use environment variables:
-  # - ARM_CLIENT_ID
-  # - ARM_CLIENT_SECRET
-  # - ARM_SUBSCRIPTION_ID
-  # - ARM_TENANT_ID
+  # OIDC authentication for GitHub Actions
+  use_oidc = true
   
-  # skip_provider_registration is recommended to avoid permission issues
+  # These are automatically populated by azure/login@v2 from GitHub Actions secrets
+  client_id       = var.azure_client_id
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  
   skip_provider_registration = false
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
+# Azure authentication variables (required for OIDC in GitHub Actions)
+variable "azure_client_id" {
+  description = "Azure Client ID for OIDC authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID for OIDC authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID for OIDC authentication"
+  type        = string
+  sensitive   = true
+}
+
 variable "resource_group_name" {
   description = "Name of the Azure resource group"
   type        = string
