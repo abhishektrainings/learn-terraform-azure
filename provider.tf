99c8b066ed0067e1e584efd953295c26ff17d385
Abhishek Tiwari <abhishektrainings30@gmail.com>
2026-01-19 08:46:46 +0530

    my new commit

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.0"
     }
   }
 }
@@ -10,8 +10,14 @@ terraform {
 provider "azurerm" {
   features {}
 
-  # OIDC authentication via environment variables
-  # ARM_CLIENT_ID, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID, ARM_OIDC_TOKEN, ARM_USE_OIDC
+  # Explicitly set credentials for OIDC authentication
+  client_id       = var.azure_client_id
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
   
+  # Use OIDC token from GitHub Actions
+  oidc_token = var.azure_oidc_token
+  use_oidc   = true
+
   skip_provider_registration = false
 }
